7984c6515b39d3d0e360b1b5073f96218c1cb87c
Murilo <murilobdo@hotmail.com>
2022-07-16 22:19:16 -0300

    Inserindo a data e hora na mensagem

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace webapi.Controllers
+{
+    public class HomeController : ControllerBase
+    {
+        [HttpGet("/")]
+        public IActionResult Get()
+            => Ok($"Hello world {DateTime.UtcNow}");   
+    }
+}
